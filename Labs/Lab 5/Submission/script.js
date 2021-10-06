f8db3f03fc8cb35fe087f8aba368fdff18262106
ConnorGitSave <connor.johnson1@marist.edu>
2021-10-06 16:43:20 -0400

    Finished - JS submission work

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,8 @@
+document.getElementById("title").innerHTML = "I changed the texted through Java ( ͡° ͜ʖ ͡°) ";
+document.getElementById("description").innerHTML = "Connor Johnson";
+document.getElementById("bossy").innerHTML = "You really want me to change this to be less bossy (ノಠ益ಠ)ノ彡┻━┻";
+document.getElementById("line-1").style.color = "Blue"
+document.getElementById("line-2").style.backgroundColor = "#FFFDD0";
+document.body.style.backgroundColor = "grey";
+document.getElementById("line-4").style.textTransform = "uppercase";
+document.getElementById("line-5").classList.add("title");
