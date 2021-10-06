77195f1ee022d8d5699f2c7add8acbfb234dd8a4
ConnorGitSave <connor.johnson1@marist.edu>
2021-10-06 18:49:05 -0400

    -added the correct way for js

--- Labs/Lab 5/Starter/script.js
+++ Labs/Lab 5/Starter/script.js
@@ -6,3 +6,22 @@ document.getElementById("line-2").style.backgroundColor = "#FFFDD0";
 document.body.style.backgroundColor = "grey";
 document.getElementById("line-4").style.textTransform = "uppercase";
 document.getElementById("line-5").classList.add("title");
+
+/*
+var h1 = document.querySelector("title");
+var p = document.querySelector("description");
+var h3 = document.querySelector("bossy");
+var lis = document.querySelector("li");
+var body = document.querySelector("body");
+var title = document.querySelector("title");
+
+history.textContent = "Changed Title";
+p.textContent ="Connor";
+h3.textContent = "less Bossy";
+title.textContent = "Look at me";
+
+lis[0].style.color = "Blue";
+lis[1].style.backgroundColor = "Pink";
+body.style.backgroundColor = "grey";
+lis[3].style.textTransform = "uppercase";
+lis[4].classList.add("title");/*
